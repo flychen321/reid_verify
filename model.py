4d07f0b9f91a9276431185a2f9087782258fc882
dl <571435345@qq.com>
2019-01-31 16:28:34 +0800

    temp commit

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ from torch.nn import init
 from torchvision import models
 from torch.autograd import Variable
 
+
 ######################################################################
 def weights_init_kaiming(m):
     classname = m.__class__.__name__
@@ -17,11 +18,13 @@ def weights_init_kaiming(m):
         init.normal_(m.weight.data, 1.0, 0.02)
         init.constant_(m.bias.data, 0.0)
 
+
 def weights_init_classifier(m):
     classname = m.__class__.__name__
     if classname.find('Linear') != -1:
         init.normal_(m.weight.data, std=0.001)
         init.constant_(m.bias.data, 0.0)
+
 
 # Defines the new fc layer and classification layer
 # |--Linear--|--bn--|--relu--|--Linear--|
@@ -29,7 +32,7 @@ class ClassBlock(nn.Module):
     def __init__(self, input_dim, class_num, dropout=0.5, relu=False, num_bottleneck=512):
         super(ClassBlock, self).__init__()
         add_block = []
-        add_block += [nn.Linear(input_dim, num_bottleneck)] 
+        add_block += [nn.Linear(input_dim, num_bottleneck)]
         add_block += [nn.BatchNorm1d(num_bottleneck)]
         if relu:
             add_block += [nn.LeakyReLU(0.1)]
@@ -37,35 +40,38 @@ class ClassBlock(nn.Module):
         add_block.apply(weights_init_kaiming)
 
         classifier = []
-        if dropout>0:
-            classifier+= [nn.Dropout(p=dropout)]
+        if dropout > 0:
+            classifier += [nn.Dropout(p=dropout)]
         classifier += [nn.Linear(num_bottleneck, class_num)]
         classifier = nn.Sequential(*classifier)
         classifier.apply(weights_init_classifier)
 
         self.add_block = add_block
         self.classifier = classifier
+
     def forward(self, x):
         x = self.add_block(x)
         f = x
         f_norm = f.norm(p=2, dim=1, keepdim=True) + 1e-8
         f = f.div(f_norm)
         x = self.classifier(x)
-        return x,f
+        return x, f
+
 
 # Define the ResNet50-based Model
 class ft_net(nn.Module):
 
-    def __init__(self, class_num ):
+    def __init__(self, class_num):
         super(ft_net, self).__init__()
         model_ft = models.resnet50(pretrained=True)
         # avg pooling to global pooling
-        model_ft.avgpool = nn.AdaptiveAvgPool2d((1,1))
+        model_ft.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.model = model_ft
         self.classifier = ClassBlock(2048, class_num, dropout=0.5, relu=False)
         # remove the final downsample
         # self.model.layer4[0].downsample[0].stride = (1,1)
         # self.model.layer4[0].conv2.stride = (1,1)
+
     def forward(self, x):
         x = self.model.conv1(x)
         x = self.model.bn1(x)
@@ -78,24 +84,27 @@ class ft_net(nn.Module):
         x = self.model.avgpool(x)
         x = x.view(x.size(0), x.size(1))
         x, f = self.classifier(x)
-        return x,f
+        return x, f
+
 
 # Define a 2048 to 2 Model
 class verif_net(nn.Module):
     def __init__(self):
         super(verif_net, self).__init__()
         self.classifier = ClassBlock(512, 2, dropout=0.75, relu=False)
+
     def forward(self, x):
         x = self.classifier.classifier(x)
         return x
 
+
 # Define the DenseNet121-based Model
 class ft_net_dense(nn.Module):
 
-    def __init__(self, class_num ):
+    def __init__(self, class_num):
         super().__init__()
         model_ft = models.densenet121(pretrained=True)
-        model_ft.features.avgpool = nn.AdaptiveAvgPool2d((1,1))
+        model_ft.features.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         model_ft.fc = nn.Sequential()
         self.model = model_ft
         # For DenseNet, the feature dim is 1024 
@@ -106,18 +115,19 @@ class ft_net_dense(nn.Module):
         x = x.view(x.size(0), x.size(1))
         x = self.classifier(x)
         return x
-    
+
+
 # Define the ResNet50-based Model (Middle-Concat)
 # In the spirit of "The Devil is in the Middle: Exploiting Mid-level Representations for Cross-Domain Instance Matching." Yu, Qian, et al. arXiv:1711.08106 (2017).
 class ft_net_middle(nn.Module):
 
-    def __init__(self, class_num ):
+    def __init__(self, class_num):
         super(ft_net_middle, self).__init__()
         model_ft = models.resnet50(pretrained=True)
         # avg pooling to global pooling
-        model_ft.avgpool = nn.AdaptiveAvgPool2d((1,1))
+        model_ft.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.model = model_ft
-        self.classifier = ClassBlock(2048+1024, class_num)
+        self.classifier = ClassBlock(2048 + 1024, class_num)
 
     def forward(self, x):
         x = self.model.conv1(x)
@@ -132,27 +142,28 @@ class ft_net_middle(nn.Module):
         x = self.model.layer4(x)
         # x1  n*2048*1*1
         x1 = self.model.avgpool(x)
-        x = torch.cat((x0,x1),1)
+        x = torch.cat((x0, x1), 1)
         x = torch.squeeze(x)
         x = self.classifier(x)
         return x
 
+
 # Part Model proposed in Yifan Sun etal. (2018)
 class PCB(nn.Module):
-    def __init__(self, class_num ):
+    def __init__(self, class_num):
         super(PCB, self).__init__()
 
-        self.part = 6 # We cut the pool5 to 6 parts
+        self.part = 6  # We cut the pool5 to 6 parts
         model_ft = models.resnet50(pretrained=True)
         self.model = model_ft
-        self.avgpool = nn.AdaptiveAvgPool2d((self.part,1))
+        self.avgpool = nn.AdaptiveAvgPool2d((self.part, 1))
         self.dropout = nn.Dropout(p=0.5)
         # remove the final downsample
-        self.model.layer4[0].downsample[0].stride = (1,1)
-        self.model.layer4[0].conv2.stride = (1,1)
+        self.model.layer4[0].downsample[0].stride = (1, 1)
+        self.model.layer4[0].conv2.stride = (1, 1)
         # define 6 classifiers
         for i in range(self.part):
-            name = 'classifier'+str(i)
+            name = 'classifier' + str(i)
             setattr(self, name, ClassBlock(2048, class_num, True, False, 256))
 
     def forward(self, x):
@@ -160,7 +171,7 @@ class PCB(nn.Module):
         x = self.model.bn1(x)
         x = self.model.relu(x)
         x = self.model.maxpool(x)
-        
+
         x = self.model.layer1(x)
         x = self.model.layer2(x)
         x = self.model.layer3(x)
@@ -171,29 +182,30 @@ class PCB(nn.Module):
         predict = {}
         # get six part feature batchsize*2048*6
         for i in range(self.part):
-            part[i] = torch.squeeze(x[:,:,i])
-            name = 'classifier'+str(i)
-            c = getattr(self,name)
+            part[i] = torch.squeeze(x[:, :, i])
+            name = 'classifier' + str(i)
+            c = getattr(self, name)
             predict[i] = c(part[i])
 
         # sum prediction
-        #y = predict[0]
-        #for i in range(self.part-1):
+        # y = predict[0]
+        # for i in range(self.part-1):
         #    y += predict[i+1]
         y = []
         for i in range(self.part):
             y.append(predict[i])
         return y
 
+
 class PCB_test(nn.Module):
-    def __init__(self,model):
-        super(PCB_test,self).__init__()
+    def __init__(self, model):
+        super(PCB_test, self).__init__()
         self.part = 6
         self.model = model.model
-        self.avgpool = nn.AdaptiveAvgPool2d((self.part,1))
+        self.avgpool = nn.AdaptiveAvgPool2d((self.part, 1))
         # remove the final downsample
-        self.model.layer4[0].downsample[0].stride = (1,1)
-        self.model.layer4[0].conv2.stride = (1,1)
+        self.model.layer4[0].downsample[0].stride = (1, 1)
+        self.model.layer4[0].conv2.stride = (1, 1)
 
     def forward(self, x):
         x = self.model.conv1(x)
@@ -206,14 +218,14 @@ class PCB_test(nn.Module):
         x = self.model.layer3(x)
         x = self.model.layer4(x)
         x = self.avgpool(x)
-        y = x.view(x.size(0),x.size(1),x.size(2))
+        y = x.view(x.size(0), x.size(1), x.size(2))
         return y
 
 # debug model structure
-#net = ft_net(751)
-#net = ft_net(751)
-#print(net)
-#input = Variable(torch.FloatTensor(8, 3, 224, 224))
-#output,f = net(input)
-#print('net output size:')
-#print(f.shape)
+# net = ft_net(751)
+# net = ft_net(751)
+# print(net)
+# input = Variable(torch.FloatTensor(8, 3, 224, 224))
+# output,f = net(input)
+# print('net output size:')
+# print(f.shape)
--- run.py
+++ run.py
@@ -2,15 +2,15 @@ import os
 import numpy as np
 
 value = [[0, 100], [70, 90], [60, 80], [60, 90], [50, 100], [80, 90], [70, 80], [80, 80], [90, 90], ]
-for i in np.arange(3):
+for i in np.arange(1):
     print('i = %.3f' % i)
     log_name = 'log/' + 'log_' + str(i)
     print('log name = %s' % log_name)
     cmd = 'python train.py --net_loss_model ' + str(
-        i) + ' --use_dense --gpu_ids 0 --name ft_ResNet50 --train_all --batchsize 24  --erasing_p 0.5' + ' >> ' + log_name
+        i) + ' --use_dense --gpu_ids 0 --name ft_DesNet --train_all --batchsize 32  --erasing_p 0.5' + ' >> ' + log_name
     print('cmd = %s' % cmd)
     os.system(cmd)
 
-    os.system('python test.py  --use_dense --gpu_ids 0 --name ft_ResNet50  --which_epoch 59 ' + ' >>  ' + log_name)
+    os.system('python test.py  --use_dense --gpu_ids 0 --name ft_DesNet  --which_epoch 59 ' + ' >>  ' + log_name)
     os.system('python evaluate_gpu.py' + ' >> ' + log_name)
     os.system('python evaluate_rerank.py' + ' >> ' + log_name)
