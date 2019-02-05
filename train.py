e45efbd166ccf2112936055baef65f63d4267c42
dl <571435345@qq.com>
2019-02-05 12:54:08 +0800

    set satge1 True stag2 False

--- train.py
+++ train.py
@@ -594,8 +594,8 @@ if not os.path.isdir(dir_name):
 with open('%s/opts.yaml' % dir_name, 'w') as fp:
     yaml.dump(vars(opt), fp, default_flow_style=False)
 
-stage_1 = False
-stage_2 = True
+stage_1 = True
+stage_2 = False
 
 if stage_1:
     # train_model = train_model_triplet
